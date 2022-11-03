5d8fe0b1fc3515b6f2ae259b9cb4d21f8d2adb54
u48gmv <32902834+u48gmv@users.noreply.github.com>
2022-11-03 18:17:16 +0100

    Learning Hooks

--- App.tsx
+++ App.tsx
@@ -1,12 +1,46 @@
-import * as React from 'react';
+import React, { useState, useEffect, createElement } from 'react';
 import './style.css';
 
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('');
+  const [todos, setTodos] = useState([
+    { title: 'title todo 1', description: 'description todo 1' },
+    { title: 'title todo 1', description: 'description todo 1' },
+  ]);
 
-export default function App() {
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    setTitle('This is a new title on load');
+  });
+
+  const GenerateList = () => {
+    const toDoListLen = todos.length;
+    let listItems = [];
+    let list = createElement('ul', null, listItems);
+
+    for (let i = 0; i < toDoListLen; i++) {
+      listItems.push(createElement('li', null, `${todos[i].title}`));
+    }
+
+    return list;
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h2>{title}</h2>
+      <p>You clicked {count} times</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me
+      </button>
+      <p>{`Some nice paragraph. Click me was hit ${count} times`}</p>
+      {GenerateList()}
     </div>
   );
-}
+};
+
+export default App;
